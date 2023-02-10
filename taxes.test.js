58d179059dc0d0c5bd8614e2c941559818b0e786
shane <veoseeo@gmail.com>
2023-02-10 07:50:40 -0800

    modifed the tax rate

--- taxes.test.js
+++ taxes.test.js
@@ -1,8 +1,10 @@
 it('should calculate the high tax bracket', function () {
-    expect(1 + 1).toEqual(2);
-    expect('hello').toEqual('hello');
+    expect(calcaulateTaxes(50000)).toEqual(12500);
+    expect(calcaulateTaxes(100000)).toEqual(25000);
 
+   })
+it('should calculate the low tax bracket', function () {
+    expect(calcaulateTaxes(10000)).toEqual(1500);
+    expect(calcaulateTaxes(1000)).toEqual(150);
+    expect(calcaulateTaxes(0)).toEqual(0);
 })
-//it('should calculate the low tax bracket', function () {
-
-//})
